fb76891c1f03d8936f843669194bb082fbbaeeeb
Devika Pradhan <dpradha@purdue.edu>
2018-09-13 17:03:40 -0400

    Changes to set the QA, staging and prod slots on az

---
+++ routes/constants/index.js
@@ -0,0 +1,2 @@
+export const INDEX_ROUTE = 'index';
+export const PAGE_TITLE_LABEL = 'DKO';
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+import * as index from './constants/index'
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var model = {
+    title: 'Azure testing',
+    message: process.env.message || "Development",
+  };
+  res.render(index.INDEX_ROUTE, model);
 });
 
 module.exports = router;
