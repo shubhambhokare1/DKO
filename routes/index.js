10a2f30d615934f796f6df88d3cd6e91af19e368
Devika Pradhan <dpradha@purdue.edu>
2018-09-13 23:12:17 -0400

    Setting Prod right

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
-import * as index from './constants/index'
+import * as INDEX from './constants/index'
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var model = {
-    title: 'Azure testing',
-    message: process.env.MESSAGE || "Development",
-  };
-  res.render(index.INDEX_ROUTE, model);
+  // var model = {
+  //   title: 'Azure testing',
+  //   message: process.env.MESSAGE || "Development",
+  // };
+  // res.render(index.INDEX_ROUTE, model);
+
+  res.render(INDEX.INDEX_ROUTE, {title: INDEX.PAGE_TITLE_LABEL});
 });
 
 module.exports = router;
