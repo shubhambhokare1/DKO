7b08a459bd70fbc2f412f407150892231a3ccac5
Devika Pradhan <dpradha@purdue.edu>
2018-09-14 17:56:41 -0400

    Fixed master

--- routes/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const INDEX_ROUTE = 'index';
-export const PAGE_TITLE_LABEL = 'DKO';
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
-import * as INDEX from './constants/index'
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // var model = {
-  //   title: 'Azure testing',
-  //   message: process.env.MESSAGE || "Development",
-  // };
-  // res.render(index.INDEX_ROUTE, model);
-
-  res.render(INDEX.INDEX_ROUTE, {title: INDEX.PAGE_TITLE_LABEL});
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
-l
