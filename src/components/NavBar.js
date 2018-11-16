a16d9826c4d72a48ac89b5905c19095314665b4f
Devika Pradhan <dpradha@purdue.edu>
2018-11-16 14:06:07 -0500

    Nav bar routes set and box shadow issue fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Anchor, Box, Image, Grommet} from 'grommet';
+import {RoutedAnchor, Box, Image, Grommet} from 'grommet';
 import {MENU_OPTIONS} from '../constants/pages'
 import {NAV_BAR_THEME} from '../constants/themes';
 
@@ -9,10 +9,10 @@ export const NavBar = () => (
             <Box basis='xxsmall' height='xxsmall'>
                 <Image src='dko.png'/>
             </Box>
-            <Anchor label={MENU_OPTIONS[0]} onClick={() => { }} margin='small'/>
-            <Anchor label={MENU_OPTIONS[1]} onClick={() => { }} margin='small'/>
-            <Anchor label={MENU_OPTIONS[2]} onClick={() => { }} margin='small'/>
-            <Anchor label={MENU_OPTIONS[3]} onClick={() => { }} margin='small'/>
+            <RoutedAnchor label={MENU_OPTIONS[0]} path='/' margin='small'/>
+            <RoutedAnchor label={MENU_OPTIONS[1]} path='/about' margin='small'/>
+            <RoutedAnchor label={MENU_OPTIONS[2]} margin='small'/>
+            <RoutedAnchor label={MENU_OPTIONS[3]} path='/dashboard' margin='small'/>
         </Box>
     </Grommet>
     
